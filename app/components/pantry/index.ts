9490092ca4ee759c7e3a7f4bc68a42f2cbbab4a1
Ari Mendelow <arimendelow@clawdbot.noodlefish-goanna.ts.net>
2026-02-16 22:13:36 -0800

    feat: add swiss-alpine toast system and wire to add-to-list flow

--- app/components/pantry/index.ts
+++
@@ -1,3 +0,0 @@
-export { BioCard, type BioCardProps } from './BioCard'
-export { RecipeGrid, type RecipeGridProps, type PantryRecipeCard } from './RecipeGrid'
-export { PantryPage, type PantryPageProps } from './PantryPage'
